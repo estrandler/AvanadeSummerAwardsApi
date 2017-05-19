c6436acdd105ecaf8b6cb421a0214df83403b34d
Erik Strandler <erik.strandler@avanade.com>
2017-05-19 21:03:56 +0200

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
- config.host = process.env.HOST || "[YOUR URI HERE]";
- config.authKey = process.env.AUTH_KEY || "[YOUR KEY HERE]";
+ config.host = process.env.HOST || "https://avanadesummerawards.documents.azure.com:443/";
+ config.authKey = process.env.AUTH_KEY || "GJ9ql8ywVWlJb8VwyRCZrTSevC5h1sOcZkeRBmaeVfsVRz26SgFYHt95MxA52j0kMJb5j9UZYBrMn80NXJ4wSA==";
  config.databaseId = "avanadesummerawards";
  config.collectionId = "Category";
 
